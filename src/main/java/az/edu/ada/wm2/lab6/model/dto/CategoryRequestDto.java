0c2ddf872df5519a73d37703e2f3afc3e1bcb79b
rzaalasgar <rza.alasgarli.23@gmail.com>
2026-04-02 18:51:56 +0400

    Add DTOs

---
+++ src/main/java/az/edu/ada/wm2/lab6/model/dto/CategoryRequestDto.java
@@ -0,0 +1,20 @@
+package az.edu.ada.wm2.lab6.model.dto;
+
+public class CategoryRequestDto {
+    private String name;
+
+    public CategoryRequestDto() {
+    }
+
+    public CategoryRequestDto(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
